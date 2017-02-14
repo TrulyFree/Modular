107b371b2d6a8e13436c07bd8c43bb665f16269d
VTCAKAVSMoACE <vtcakavsmoace@gmail.com>
2017-02-14 18:44:52 +0000

    Made mvnandpush.sh generic to mvn build types.

--- buildscripts/mvnandpush.sh
+++ buildscripts/mvnandpush.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 TARGET_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
+MVN_OPTIONS_PROMPT="Maven arguments? "
 MSG_PROMPT="Commit message? "
 BRANCH_PROMPT="Branch? "
 
 cd "$TARGET_DIR"
 
-mvn test
+echo -n "$MVN_OPTIONS_PROMPT"
+read mvn_opt
+
+mvn $mvn_opt
 
 echo -n "$MSG_PROMPT"
 read msg
