c8316f7f70b0d4fd1833208b115650265c55a98c
VTCAKAVSMoACE <vtcakavsmoace@gmail.com>
2017-02-15 20:37:50 +0000

    Write Event tests

---
+++ src/test/java/io/github/trulyfree/modular/test/event/EventTest.java
@@ -0,0 +1,91 @@
+package io.github.trulyfree.modular.test.event;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import io.github.trulyfree.modular.event.Event;
+
+/* Modular library by TrulyFree: A general-use module-building library.
+ * Copyright (C) 2016  VTCAKAVSMoACE
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class EventTest {
+
+	private static int modified;
+
+	private static Event event1;
+	private static Event event2;
+
+	@BeforeClass
+	public static void setup() {
+		modified = 0;
+
+		event1 = new Event() {
+			@Override
+			public boolean enact() {
+				modified = 1;
+				return true;
+			}
+		};
+
+		event2 = new Event() {
+			@Override
+			public boolean enact() {
+				modified = 0;
+				return true;
+			}
+		};
+	}
+
+	@Test
+	public void stage0_verifyNoAction() {
+		assertEquals(0, modified);
+	}
+
+	@Test
+	public void stage1_testEnact() {
+		assertTrue(event1.enact());
+	}
+
+	@Test
+	public void stage2_verifyEnact() {
+		assertEquals(1, modified);
+	}
+
+	@Test
+	public void stage3_testEnact() {
+		assertTrue(event2.enact());
+	}
+
+	@Test
+	public void stage4_verifyEnact() {
+		assertEquals(0, modified);
+	}
+
+	@AfterClass
+	public static void destroy() {
+		modified = 0;
+		event1 = null;
+		event2 = null;
+	}
+
+}
