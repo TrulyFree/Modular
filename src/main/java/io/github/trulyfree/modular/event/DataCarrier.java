a19fb4be85c95aa7c97154a5466808c76b7f080b
VTCAKAVSMoACE <vtcakavsmoace@gmail.com>
2017-02-19 16:42:53 +0000

    Commentify DataCarrier interfae

--- src/main/java/io/github/trulyfree/modular/event/DataCarrier.java
+++ src/main/java/io/github/trulyfree/modular/event/DataCarrier.java
@@ -17,10 +17,34 @@ package io.github.trulyfree.modular.event;
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * DataCarrier interface. This interface should be implemented by anything that
+ * carries data between one module/event to another asynchronously.
+ * 
+ * @author vtcakavsmoace
+ *
+ * @param <T>
+ *            The type of data being passed.
+ */
 public interface DataCarrier<T> {
 
+	/**
+	 * Method to be called in order to gather the data carried by this instance.
+	 * 
+	 * @return data The data carried by this instance.
+	 */
 	public T getData();
-	
+
+	/**
+	 * Method to be called in order to set the data carried by this instance.
+	 * Note that this may have no effect depending on the implmenation.
+	 * 
+	 * @param data
+	 *            The data to be carried by this instance.
+	 * @return success A boolean representing whether or not the data was set to
+	 *         the passed value. This should return false if the data may not be
+	 *         altered.
+	 */
 	public boolean setData(T data);
-	
+
 }
