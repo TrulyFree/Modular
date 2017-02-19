fb626be64f8c804c5961ba9c5371fc36db9e9df2
VTCAKAVSMoACE <vtcakavsmoace@gmail.com>
2017-02-19 16:30:05 +0000

    Commentify Cancellable interface.

--- src/main/java/io/github/trulyfree/modular/event/Cancellable.java
+++ src/main/java/io/github/trulyfree/modular/event/Cancellable.java
@@ -17,9 +17,37 @@ package io.github.trulyfree.modular.event;
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * Cancellable interface. All cancellable events or modules should implement
+ * this interface. It is highly recommended that all implementations of this
+ * class prevent any form of interaction with the instance while the instance is
+ * in the cancelled state.
+ * 
+ * @author vtcakavsmoace
+ *
+ */
 public interface Cancellable {
 
+	/**
+	 * Method to be called in order to set the cancelled state of this instance.
+	 * Should be called with cancelled set to true to cancel the instance, and
+	 * false as not cancelled.
+	 * 
+	 * @param cancelled
+	 *            A boolean representing the cancelled state of this instance.
+	 * @return success A boolean representing the success of this operation.
+	 *         This has essentially no reason to be false.
+	 */
 	public boolean setCancelled(boolean cancelled);
+
+	/**
+	 * Method to be called in order to check the cancelled state of this
+	 * instance. Should return true if the instance is cancelled, false if
+	 * otherwise.
+	 * 
+	 * @return cancelled A boolean representing the cancelled state of this
+	 *         instance.
+	 */
 	public boolean isCancelled();
-	
+
 }
