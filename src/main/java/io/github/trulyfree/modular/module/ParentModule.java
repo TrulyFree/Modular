73e85d329785cf2fa9648779b64a92be25022b89
VTCAKAVSMoACE <vtcakavsmoace@gmail.com>
2017-02-19 16:10:57 +0000

    Commentify ParentModule interface

--- src/main/java/io/github/trulyfree/modular/module/ParentModule.java
+++ src/main/java/io/github/trulyfree/modular/module/ParentModule.java
@@ -1,6 +1,6 @@
 package io.github.trulyfree.modular.module;
 
-import java.util.List;
+import java.util.Collection;
 
 /* Modular library by TrulyFree: A general-use module-building library.
  * Copyright (C) 2016  VTCAKAVSMoACE
@@ -19,8 +19,32 @@ import java.util.List;
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * ParentModule interface. All modules with multiple submodules should implement
+ * this interface. It is highly recommended that all implementations of this
+ * class use only non-final instance fields as to assure the usefulness of the
+ * setup and destroy methods of the Module interface from which this interface
+ * extends.
+ * 
+ * @author vtcakavsmoace
+ *
+ * @param <T>
+ *            The module type supported by this ParentModule. It is likely that
+ *            most implementations will generalize this to Module, but
+ *            implementations may vary.
+ */
 public interface ParentModule<T extends Module> extends Module {
-	
-	List<T> getChildren();	
-	
+
+	/**
+	 * Method to be called in order to retrieve the children of this interface.
+	 * The collection returned should NOT be capable of modifying the module's
+	 * child modules.
+	 * 
+	 * @return children The child modules of this module. Note that
+	 *         modifications this collection should NOT modify the children of
+	 *         the parent module. Instead, should this behavior be intended, the
+	 *         user should implement the ModifiableParentModule interface.
+	 */
+	Collection<T> getChildren();
+
 }
