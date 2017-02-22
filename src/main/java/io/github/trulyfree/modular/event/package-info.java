674a8646c252d7749c5f5589d868790356f69e62
VTCAKAVSMoACE <vtcakavsmoace@gmail.com>
2017-02-22 09:02:14 +0000

    Add package-info.java to each package

---
+++ src/main/java/io/github/trulyfree/modular/event/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Modular's Event package. Defines interfaces with direct relevance to events
+ * and their handling process.
+ * 
+ * @see io.github.trulyfree.modular.event.Event
+ */
+package io.github.trulyfree.modular.event;
---
+++ src/main/java/io/github/trulyfree/modular/module/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Modular's Modules package. Defines interfaces with direct relevance to
+ * modules and their handling process.
+ * 
+ * @see io.github.trulyfree.modular.module.Module;
+ */
+package io.github.trulyfree.modular.module;
