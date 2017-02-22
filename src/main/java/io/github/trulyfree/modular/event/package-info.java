e71b90898187cff5d8a84427fd47f6eb94b8a61f
VTCAKAVSMoACE <vtcakavsmoace@gmail.com>
2017-02-22 09:03:50 +0000

    Rebuild javadocs after failed build

--- src/main/java/io/github/trulyfree/modular/event/package-info.java
+++ src/main/java/io/github/trulyfree/modular/event/package-info.java
@@ -1,7 +1,5 @@
 /**
  * Modular's Event package. Defines interfaces with direct relevance to events
  * and their handling process.
- * 
- * @see io.github.trulyfree.modular.event.Event
  */
 package io.github.trulyfree.modular.event;
