2c8b8cb295aa7e14c20ff25ae65ec1d4894d0808
VTCAKAVSMoACE <vtcakavsmoace@gmail.com>
2017-02-25 22:07:16 +0000

    Commentify DisplayableException

--- src/main/java/io/github/trulyfree/modular/display/except/DisplayableException.java
+++ src/main/java/io/github/trulyfree/modular/display/except/DisplayableException.java
@@ -5,13 +5,19 @@ import io.github.trulyfree.modular.display.Displayable;
 /**
  * UnsupportedDisplayableException class. This exception should be thrown
  * whenever a Display does not support a Displayable instance provided by the
- * DisplayableModule that is marked as "necessary". You MUST provide a
- * Displayable instance in order to throw this Exception.
+ * DisplayableModule that is below the Display's priority threshold OR when a
+ * Displayable instance fails to be rendered. You MUST provide a Displayable
+ * instance in order to throw this Exception.
  * 
  * @author vtcakavsmoace
  *
  */
-public class UnsupportedDisplayableException extends Exception {
+public class DisplayableException extends Exception {
+
+	/**
+	 * Because I'm supposed to.
+	 */
+	private static final long serialVersionUID = -6212968576724328187L;
 
 	/**
 	 * Standard displayable constructor, which instantiates with the message
@@ -22,53 +28,60 @@ public class UnsupportedDisplayableException extends Exception {
 	 * @param disp
 	 *            The displayable which caused the exception.
 	 */
-	public UnsupportedDisplayableException(Displayable disp) {
+	public DisplayableException(Displayable disp) {
 		this("Unknown or unsupported displayable", disp);
 	}
 
 	/**
 	 * @param message
+	 *            The message detailing the event leading to the exception.
 	 * @param disp
 	 *            The displayable which caused the exception.
 	 */
-	public UnsupportedDisplayableException(String message, Displayable disp) {
+	public DisplayableException(String message, Displayable disp) {
 		super(messagify(message, disp));
 	}
 
 	/**
 	 * @param cause
+	 *            The throwable which caused the exception to be thrown.
 	 * @param disp
 	 *            The displayable which caused the exception.
 	 */
-	public UnsupportedDisplayableException(Throwable cause, Displayable disp) {
+	public DisplayableException(Throwable cause, Displayable disp) {
 		this("Unknown or unsupported displayable", cause, disp);
 	}
 
 	/**
 	 * @param message
+	 *            The message detailing the event leading to the exception.
 	 * @param cause
+	 *            The throwable which caused the exception to be thrown.
 	 * @param disp
 	 *            The displayable which caused the exception.
 	 */
-	public UnsupportedDisplayableException(String message, Throwable cause, Displayable disp) {
+	public DisplayableException(String message, Throwable cause, Displayable disp) {
 		super(messagify(message, disp), cause);
 	}
 
 	/**
 	 * @param message
+	 *            The message detailing the event leading to the exception.
 	 * @param cause
-	 * @param enableSuppression
-	 * @param writableStackTrace
+	 *            The throwable which caused the exception to be thrown.
+	 * @param enableSuppression whether or not suppression is enabled or disabled
+	 * @param writableStackTrace whether or not the stack trace should be writable
 	 * @param disp
 	 *            The displayable which caused the exception.
 	 */
-	public UnsupportedDisplayableException(String message, Throwable cause, boolean enableSuppression,
+	public DisplayableException(String message, Throwable cause, boolean enableSuppression,
 			boolean writableStackTrace, Displayable disp) {
 		super(messagify(message, disp), cause, enableSuppression, writableStackTrace);
 	}
 
 	/**
 	 * @param message
+	 *            The message detailing the event leading to the exception.
 	 * @param disp
 	 *            The displayable which caused the exception.
 	 * @return fullMessage The message to be used by the Exception.
