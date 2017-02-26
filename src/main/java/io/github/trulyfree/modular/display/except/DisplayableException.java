8505b24a965defdb86905536018dedc39fc4a787
VTCAKAVSMoACE <vtcakavsmoace@gmail.com>
2017-02-26 10:52:42 +0000

    Licensify Displayable related items

--- src/main/java/io/github/trulyfree/modular/display/except/DisplayableException.java
+++ src/main/java/io/github/trulyfree/modular/display/except/DisplayableException.java
@@ -2,6 +2,23 @@ package io.github.trulyfree.modular.display.except;
 
 import io.github.trulyfree.modular.display.Displayable;
 
+/* Modular library by TrulyFree: A general-use module-building library.
+ * Copyright (C) 2016  VTCAKAVSMoACE
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
  * UnsupportedDisplayableException class. This exception should be thrown
  * whenever a Display does not support a Displayable instance provided by the
@@ -69,13 +86,15 @@ public class DisplayableException extends Exception {
 	 *            The message detailing the event leading to the exception.
 	 * @param cause
 	 *            The throwable which caused the exception to be thrown.
-	 * @param enableSuppression whether or not suppression is enabled or disabled
-	 * @param writableStackTrace whether or not the stack trace should be writable
+	 * @param enableSuppression
+	 *            whether or not suppression is enabled or disabled
+	 * @param writableStackTrace
+	 *            whether or not the stack trace should be writable
 	 * @param disp
 	 *            The displayable which caused the exception.
 	 */
-	public DisplayableException(String message, Throwable cause, boolean enableSuppression,
-			boolean writableStackTrace, Displayable disp) {
+	public DisplayableException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace,
+			Displayable disp) {
 		super(messagify(message, disp), cause, enableSuppression, writableStackTrace);
 	}
 
