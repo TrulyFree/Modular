e914d953c7aea10c8285626d562dbc6e30e1e8f9
VTCAKAVSMoACE <vtcakavsmoace@gmail.com>
2017-02-26 20:07:06 +0000

    Final licensifications

--- src/test/java/io/github/trulyfree/modular/test/integrate/impl/SimpleForkableEvent.java
+++ src/test/java/io/github/trulyfree/modular/test/integrate/impl/SimpleForkableEvent.java
@@ -7,6 +7,23 @@ import io.github.trulyfree.modular.general.Forkable;
 import io.github.trulyfree.modular.test.event.impl.SimpleEventGroup;
 import io.github.trulyfree.modular.test.integrate.ForkableEventTest;
 
+/* Modular library by TrulyFree: A general-use module-building library.
+ * Copyright (C) 2016  VTCAKAVSMoACE
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 public class SimpleForkableEvent extends SimpleEventGroup<Event> implements Forkable, Event {
 
 	private boolean halted;
