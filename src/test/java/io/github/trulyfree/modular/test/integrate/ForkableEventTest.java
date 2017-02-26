6fc8b040da3250873a3756c6979198495a367b44
VTCAKAVSMoACE <vtcakavsmoace@gmail.com>
2017-02-26 19:30:05 +0000

    Write some of ForkableEventTest

---
+++ src/test/java/io/github/trulyfree/modular/test/integrate/ForkableEventTest.java
@@ -0,0 +1,64 @@
+package io.github.trulyfree.modular.test.integrate;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import io.github.trulyfree.modular.event.Event;
+import io.github.trulyfree.modular.test.integrate.impl.SimpleForkableEvent;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ForkableEventTest {
+
+	private static SimpleForkableEvent sfe;
+
+	public static boolean finished;
+
+	@Test
+	public void stage0_0_setup() {
+		List<Event> events = new ArrayList<Event>();
+		events.add(new Event() {
+
+			@Override
+			public boolean enact() {
+				sfe.increment();
+				return true;
+			}
+
+		});
+		sfe = new SimpleForkableEvent(events);
+	}
+
+	@Test
+	public void stage0_1_verifyNoAction() {
+		assertFalse(finished);
+		assertEquals(0, sfe.value);
+	}
+
+	@Test
+	public void stage1_0_testSafeHalt() {
+		assertTrue(sfe.enact());
+		synchronized (sfe) {
+			try {
+				sfe.wait();
+				assertTrue(sfe.safeHalt());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Test
+	public void stage2_1_verifySafeHalt() {
+		assertTrue(sfe.value > 10000);
+		assertTrue(finished);
+	}
+
+}
--- src/test/java/io/github/trulyfree/modular/test/integrate/impl/SimpleForkableEvent.java
+++ src/test/java/io/github/trulyfree/modular/test/integrate/impl/SimpleForkableEvent.java
@@ -5,19 +5,46 @@ import java.util.List;
 import io.github.trulyfree.modular.event.Event;
 import io.github.trulyfree.modular.general.Forkable;
 import io.github.trulyfree.modular.test.event.impl.SimpleEventGroup;
+import io.github.trulyfree.modular.test.integrate.ForkableEventTest;
 
 public class SimpleForkableEvent extends SimpleEventGroup<Event> implements Forkable, Event {
 
 	private boolean halted;
 	private Thread fork;
 
+	public int value = 0;
+
 	public SimpleForkableEvent(List<Event> events) {
 		super(events);
+		fork = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				for (;;) {
+					if (!getHalted()) {
+						enactNextEvent();
+					} else {
+						synchronized (SimpleForkableEvent.this) {
+							SimpleForkableEvent.this.notify();
+						}
+						break;
+					}
+				}
+			}
+
+		});
 	}
 
 	@Override
-	public boolean safeHalt() {
-		return halted = true;
+	public synchronized boolean safeHalt() {
+		try {
+			halted = true;
+			wait();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		ForkableEventTest.finished = true;
+		return true;
 	}
 
 	@SuppressWarnings("deprecation")
@@ -29,21 +56,23 @@ public class SimpleForkableEvent extends SimpleEventGroup<Event> implements Fork
 
 	@Override
 	public boolean enact() {
-		fork = new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				if (!getHalted()) {
-					enactNextEvent();
-				}
-			}
-
-		});
+		fork.start();
 		return true;
+	}
+
+	public Thread getFork() {
+		return fork;
 	}
 
 	private boolean getHalted() {
 		return halted;
+	}
+
+	public synchronized void increment() {
+		value++;
+		if (value > 10000) {
+			notifyAll();
+		}
 	}
 
 }
