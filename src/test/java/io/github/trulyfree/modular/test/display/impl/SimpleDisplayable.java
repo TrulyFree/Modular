6899222376438a1afe74384b13f4b1f1a859e5da
VTCAKAVSMoACE <vtcakavsmoace@gmail.com>
2017-02-26 13:48:54 +0000

    Licensify many things

--- src/test/java/io/github/trulyfree/modular/test/display/impl/SimpleDisplayable.java
+++ src/test/java/io/github/trulyfree/modular/test/display/impl/SimpleDisplayable.java
@@ -3,6 +3,23 @@ package io.github.trulyfree.modular.test.display.impl;
 import io.github.trulyfree.modular.display.Displayable;
 import io.github.trulyfree.modular.general.Priority;
 
+/* Modular library by TrulyFree: A general-use module-building library.
+ * Copyright (C) 2016  VTCAKAVSMoACE
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 public class SimpleDisplayable implements Displayable {
 
 	private Priority priority;
--- src/test/java/io/github/trulyfree/modular/test/display/impl/SimpleDisplayableModule.java
+++ src/test/java/io/github/trulyfree/modular/test/display/impl/SimpleDisplayableModule.java
@@ -4,9 +4,25 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import io.github.trulyfree.modular.display.Displayable;
 import io.github.trulyfree.modular.display.DisplayableModule;
 import io.github.trulyfree.modular.general.Priority;
+
+/* Modular library by TrulyFree: A general-use module-building library.
+ * Copyright (C) 2016  VTCAKAVSMoACE
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 public class SimpleDisplayableModule implements DisplayableModule {
 
@@ -37,7 +53,7 @@ public class SimpleDisplayableModule implements DisplayableModule {
 	}
 
 	@Override
-	public Collection<? extends Displayable> getDisplayables() {
+	public List<SimpleDisplayable> getDisplayables() {
 		return new ArrayList<SimpleDisplayable>(displayables);
 	}
 
