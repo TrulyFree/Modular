385923fc9ea42ca70d82d44a0e7d1dcae54f4e53
VTCAKAVSMoACE <vtcakavsmoace@gmail.com>
2017-03-06 08:11:12 +0000

    Licensify all the things

--- src/test/java/io/github/trulyfree/modular/test/action/handler/GeneralizedEventHandlerTest.java
+++ src/test/java/io/github/trulyfree/modular/test/action/handler/GeneralizedEventHandlerTest.java
@@ -16,36 +16,52 @@ import io.github.trulyfree.modular.action.Action;
 import io.github.trulyfree.modular.action.handlers.ActionHandler;
 import io.github.trulyfree.modular.action.handlers.GeneralizedActionHandler;
 
+/* Modular library by TrulyFree: A general-use module-building library.
+ * Copyright (C) 2016  VTCAKAVSMoACE
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class GeneralizedEventHandlerTest {
 
 	private static ActionHandler<Action> handler;
 	private static ArrayList<EventImpl> events;
-		
+
 	private static int expected;
-	
+
 	@BeforeClass
 	public static void setup() {
 		handler = new GeneralizedActionHandler();
 		events = new ArrayList<EventImpl>();
-		
-		
+
 		for (int i = 1; i <= 10; i++) {
 			events.add(new EventImpl(i));
 		}
 		expected = 1;
 	}
-	
+
 	@Test
 	public void stage0_verifyNoAction() {
 		assertFalse(handler.isReady());
 	}
-	
+
 	@Test
 	public void stage1_0_testSetup() {
 		assertTrue(handler.setup());
 	}
-	
+
 	@Test
 	public void stage1_1_verifySetup() {
 		assertTrue(handler.isReady());
@@ -54,26 +70,26 @@ public class GeneralizedEventHandlerTest {
 			assertFalse(handler.getActions().contains(event));
 		}
 	}
-	
+
 	@Test
 	public void stage2_0_testAddEvent() {
 		for (EventImpl event : events) {
 			assertTrue(handler.addEvent(event));
 		}
 	}
-	
+
 	@Test
 	public void stage2_1_verifyAddEvent() {
 		for (Action action : events) {
 			assertTrue(handler.getActions().contains(action));
 		}
 	}
-	
+
 	@Test
 	public void stage2_2_checkAddEvent() {
 		assertFalse(handler.addEvent(null));
 	}
-	
+
 	@Test
 	public void stage3_0_testAndVerifyEachEvent() {
 		for (Action action : handler.getActions()) {
@@ -81,73 +97,74 @@ public class GeneralizedEventHandlerTest {
 		}
 		assertEquals(11, expected);
 	}
-	
+
 	@Test
 	public void stage3_1_testAndVerifyEnactEachEvent() {
 		expected = 1;
-		while (handler.enactNextEvent()) {}
+		while (handler.enactNextEvent()) {
+		}
 		assertEquals(11, expected);
 	}
-	
+
 	@Test
 	public void stage3_2_verifyEnactEachEventRemoval() {
 		assertEquals(0, handler.size());
 	}
-	
+
 	@Test
 	public void stage4_0_setupStage4() {
 		stage2_0_testAddEvent();
 		expected = 1;
 	}
-	
+
 	@Test
 	public void stage4_1_testEnact() {
 		assertTrue(handler.enact());
 	}
-	
+
 	@Test
 	public void stage4_2_verifyEnactRemoval() {
 		stage3_2_verifyEnactEachEventRemoval();
 	}
-	
+
 	@Test
 	public void stage5_0_setupStage4() {
 		stage2_0_testAddEvent();
 	}
-	
+
 	@Test
 	public void stage5_1_testClear() {
 		handler.clear();
 	}
-	
+
 	@Test
 	public void stage5_2_verifyClear() {
 		stage3_2_verifyEnactEachEventRemoval();
 	}
-	
+
 	@AfterClass
 	public static void destroy() {
 		handler = null;
 		events = null;
 		expected = 0;
 	}
-	
+
 	private static void modify(int val) {
 		assertEquals(expected++, val);
 	}
-	
+
 	private static class EventImpl implements Action {
 		private final int val;
-		
+
 		public EventImpl(int val) {
 			this.val = val;
 		}
-			
+
 		@Override
 		public boolean enact() {
 			modify(val);
 			return true;
 		}
 	}
-	
+
 }
--- src/test/java/io/github/trulyfree/modular/test/action/handler/PrioritizedEventHandlerTest.java
+++ src/test/java/io/github/trulyfree/modular/test/action/handler/PrioritizedEventHandlerTest.java
@@ -18,25 +18,42 @@ import io.github.trulyfree.modular.action.handlers.ActionHandler;
 import io.github.trulyfree.modular.action.handlers.PrioritizedActionHandler;
 import io.github.trulyfree.modular.general.Priority;
 
+/* Modular library by TrulyFree: A general-use module-building library.
+ * Copyright (C) 2016  VTCAKAVSMoACE
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class PrioritizedEventHandlerTest {
 
 	private static ActionHandler<PrioritizedAction> handler;
 	private static ArrayList<EventImpl> events;
-		
+
 	private static int expected;
-	
+
 	@BeforeClass
 	public static void setup() {
 		handler = new PrioritizedActionHandler();
 		events = new ArrayList<EventImpl>();
-		
+
 		for (Priority priority : Priority.values()) {
 			events.add(new EventImpl(priority.ordinal(), priority));
 		}
 		expected = Integer.MAX_VALUE;
 	}
-	
+
 	@Test
 	public void stage1_0_verifySetup() {
 		assertTrue(handler.isReady());
@@ -45,27 +62,27 @@ public class PrioritizedEventHandlerTest {
 			assertFalse(handler.getActions().contains(event));
 		}
 	}
-	
+
 	@Test
 	public void stage2_0_testAddEvent() {
 		for (EventImpl event : events) {
 			assertTrue(handler.addEvent(event));
 		}
 	}
-	
+
 	@Test
 	public void stage2_1_verifyAddEvent() {
 		for (Action action : events) {
 			assertTrue(handler.getActions().contains(action));
 		}
 	}
-	
+
 	@Test
 	public void stage2_2_checkAddEvent() {
 		assertFalse(handler.addEvent(null));
 		assertFalse(handler.addEvent(new EventImpl(0, null)));
 	}
-	
+
 	@Test
 	public void stage3_0_testAndVerifyEachEvent() {
 		for (Action action : handler.getActions()) {
@@ -73,71 +90,72 @@ public class PrioritizedEventHandlerTest {
 		}
 		assertEquals(Priority.AESTHETIC.ordinal(), expected);
 	}
-	
+
 	@Test
 	public void stage3_1_testAndVerifyEnactEachEvent() {
 		expected = Integer.MAX_VALUE;
-		while (handler.enactNextEvent()) {}
+		while (handler.enactNextEvent()) {
+		}
 		assertEquals(Priority.AESTHETIC.ordinal(), expected);
 	}
-	
+
 	@Test
 	public void stage3_2_verifyEnactEachEventRemoval() {
 		assertEquals(0, handler.size());
 	}
-	
+
 	@Test
 	public void stage4_0_setupStage4() {
 		stage2_0_testAddEvent();
 		expected = Integer.MAX_VALUE;
 	}
-	
+
 	@Test
 	public void stage4_1_testEnact() {
 		assertTrue(handler.enact());
 	}
-	
+
 	@Test
 	public void stage4_2_verifyEnactRemoval() {
 		stage3_2_verifyEnactEachEventRemoval();
 	}
-	
+
 	@Test
 	public void stage5_0_setupStage4() {
 		stage2_0_testAddEvent();
 	}
-	
+
 	@Test
 	public void stage5_1_testClear() {
 		handler.clear();
 	}
-	
+
 	@Test
 	public void stage5_2_verifyClear() {
 		stage3_2_verifyEnactEachEventRemoval();
 	}
-	
+
 	@AfterClass
 	public static void destroy() {
 		handler = null;
 		events = null;
 		expected = 0;
 	}
-	
+
 	private static void modify(int val) {
 		assertTrue(val < expected);
 		expected = val;
 	}
-	
+
 	private static class EventImpl implements PrioritizedAction {
 		private final int val;
 		private final Priority priority;
-		
+
 		public EventImpl(int val, Priority priority) {
 			this.val = val;
 			this.priority = priority;
 		}
-			
+
 		@Override
 		public boolean enact() {
 			modify(val);
@@ -149,5 +167,5 @@ public class PrioritizedEventHandlerTest {
 			return priority;
 		}
 	}
-	
+
 }
